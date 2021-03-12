411cbb4aeb41450ca138257cf6181a3a0d82c01a
demonick82 <demonick82@gmail.com>
2021-03-12 21:36:39 +0300

    2. Каркас шахматной доски [#469760]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -7,63 +7,33 @@ import ru.job4j.chess.firuges.black.BishopBlack;
 public class LogicTest {
 
     @Test
-    public void whenMoveRight() {
-        try {
-            Logic logic = new Logic();
-            logic.add(new BishopBlack(Cell.A1));
-            logic.move(Cell.A1, Cell.H8);
-        } catch (FigureNotFoundException e) {
-            System.out.println("Фигуры нет на клетке.");
-        } catch (OccupiedCellException e) {
-            System.out.println("Ячейка занята");
-        } catch (ImpossibleMoveException e) {
-            System.out.println("Пользователь двигает фигуру не по правилам шахмат");
-        }
+    public void whenMoveRight() throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.A1));
+        logic.move(Cell.A1, Cell.H8);
     }
 
-    @Test
-    public void whenMoveOccopied() {
-        try {
-            Logic logic = new Logic();
-            logic.add(new BishopBlack(Cell.A1));
-            logic.add(new BishopBlack(Cell.C3));
-            logic.move(Cell.A1, Cell.H8);
-        } catch (FigureNotFoundException e) {
-            System.out.println("Фигуры нет на клетке.");
-        } catch (OccupiedCellException e) {
-            System.out.println("Ячейка занята");
-        } catch (ImpossibleMoveException e) {
-            System.out.println("Пользователь двигает фигуру не по правилам шахмат");
-        }
+
+    @Test(expected = OccupiedCellException.class)
+    public void whenMoveOccopied() throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.A1));
+        logic.add(new BishopBlack(Cell.C3));
+        logic.move(Cell.A1, Cell.H8);
     }
 
-    @Test
-    public void whenMoveImpossible() {
-        try {
-            Logic logic = new Logic();
-            logic.add(new BishopBlack(Cell.A1));
-            logic.move(Cell.A1, Cell.A8);
-        } catch (FigureNotFoundException e) {
-            System.out.println("Фигуры нет на клетке.");
-        } catch (OccupiedCellException e) {
-            System.out.println("Ячейка занята");
-        } catch (ImpossibleMoveException e) {
-            System.out.println("Пользователь двигает фигуру не по правилам шахмат");
-        }
+
+    @Test(expected = ImpossibleMoveException.class)
+    public void whenMoveImpossible() throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.A1));
+        logic.move(Cell.A1, Cell.A8);
     }
 
-    @Test
-    public void whenMoveFigureNotFoun() {
-        try {
+    @Test (expected = FigureNotFoundException.class)
+    public void whenMoveFigureNotFound() throws FigureNotFoundException, ImpossibleMoveException, OccupiedCellException {
             Logic logic = new Logic();
             logic.add(new BishopBlack(Cell.A1));
             logic.move(Cell.B2, Cell.A8);
-        } catch (FigureNotFoundException e) {
-            System.out.println("Фигуры нет на клетке.");
-        } catch (OccupiedCellException e) {
-            System.out.println("Ячейка занята");
-        } catch (ImpossibleMoveException e) {
-            System.out.println("Пользователь двигает фигуру не по правилам шахмат");
-        }
     }
 }
